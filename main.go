53e1886cd83ae9804efb8ae6e271446f9f0425c6
asamaayako <2433047519@qq.com>
2024-10-25 17:26:06 +0800

    注释熄屏操作 挂后台监听按键操作

--- main.go
+++ main.go
@@ -31,12 +31,12 @@ func deteckeyboard() {
 	//}
 	fmt.Println("keyboard detecing")
 	hook.Register(hook.KeyDown, []string{"command", "s"}, func(e hook.Event) {
-		sleepDLLImplementation()
+		//sleepDLLImplementation()
 		fmt.Println("sleep execued")
 	}) //这个实现不阻塞 good!!!
 
 	hook.Register(hook.KeyDown, []string{"command", "p"}, func(e hook.Event) {
-		hibernateDLLImplementation()
+		//hibernateDLLImplementation()
 		fmt.Println("hibernate execued")
 	})
 
@@ -52,8 +52,54 @@ func deteckeyboard() {
 
 	s := hook.Start()
 
-	<-hook.Process(s)
+	<-hook.Process(debugprintchan(s))
 
+}
+
+func debugprintchan(s chan hook.Event) (out chan hook.Event) {
+	out = make(chan hook.Event)
+	var kindtostrng = map[uint8]string{
+		hook.KeyDown: "KeyDown",
+		hook.KeyUp:   "KeyUp",
+		hook.KeyHold: "KeyHold",
+	}
+	go func() {
+		for {
+			e, ok := <-s
+			if !ok {
+				break
+			}
+			if e.Rawcode == 91 {
+				fmt.Println(kindtostrng[e.Kind] + " command")
+			}
+			if e.Rawcode == 162 {
+				fmt.Println(kindtostrng[e.Kind] + " control")
+			}
+			if e.Rawcode == 164 {
+				fmt.Println(kindtostrng[e.Kind] + " alt")
+			}
+			if e.Rawcode == 160 {
+				fmt.Println(kindtostrng[e.Kind] + " shift")
+			}
+			if e.Rawcode == 84 {
+				fmt.Println(kindtostrng[e.Kind] + " t")
+			}
+			if e.Rawcode == 83 {
+				fmt.Println(kindtostrng[e.Kind] + " s")
+			}
+			if e.Rawcode == 80 {
+				fmt.Println(kindtostrng[e.Kind] + " p")
+			}
+			if e.Rawcode == 77 {
+				fmt.Println(kindtostrng[e.Kind] + " m")
+			}
+
+			//fmt.Println(e.String())
+			out <- e
+		}
+		close(out)
+	}()
+	return out
 }
 
 //	func deteckeyboard2() {
